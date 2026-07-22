069c0ae19c21314199424670cb1c46ef4b974bb5
jordyromero03 <jordyromerou@gmail.com>
2026-07-21 21:29:51 -0500

    feat: integrar Footer y rutas en App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
 import Home from './pages/Home';
+import About from './pages/About';
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-background">
+      <div className="min-h-screen bg-background flex flex-col">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sobre-nosotros" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
